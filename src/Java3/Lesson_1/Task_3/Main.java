96f29e31495c5c1e9a5fc1b8cfdca1661093b7e6
Vitya <V.Smolyak@seznam.cz>
2019-07-01 11:25:30 +0200

    HW_lesson_1

---
+++ src/Java3/Lesson_1/Task_3/Main.java
@@ -0,0 +1,39 @@
+package Java3.Lesson_1.Task_3;
+
+import Java3.Lesson_1.Task_3.Boxes.Box;
+import Java3.Lesson_1.Task_3.Fruits.Apple;
+import Java3.Lesson_1.Task_3.Fruits.Orange;
+
+public class Main {
+    public static void main(String[] args) {
+        Box<Apple> appleBox = new Box<>();
+        Box<Orange> orangeBox = new Box<>();
+        int n = 10;
+        int m = 9;
+
+        for (int i = 0; i < n; i++) {
+            appleBox.add(new Apple(1.1f));
+        }
+
+        for (int i = 0; i < m; i++) {
+            orangeBox.add(new Orange(1.2f));
+        }
+
+        System.out.println(appleBox.compare(orangeBox) ? "Коробки весят одинаково" : "Коробки разного веса");
+
+        Box<Apple> appleBox1 = new Box<>();
+        for (int i = 0; i < n; i++) {
+            appleBox1.add(new Apple(1.1f));
+        }
+
+        System.out.println(appleBox.compare(appleBox1));
+
+        System.out.println(appleBox1.getWeigth());
+        System.out.println(appleBox.getWeigth());
+
+        appleBox.moveToAnotherBox(appleBox1);
+
+        System.out.println(appleBox1.getWeigth());
+        System.out.println(appleBox.getWeigth());
+    }
+}
