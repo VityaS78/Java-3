216d54eff4a34ef26ab9e5bd57876ffe27db0769
Vitya <V.Smolyak@seznam.cz>
2019-07-05 14:25:25 +0200

    HW_lesson_2_zaglushka

---
+++ src/Java3/Lesson_1/Task_3/Fruits/Fruit.java
@@ -0,0 +1,12 @@
+package Java3.Lesson_1.Task_3.Fruits;
+
+public abstract class Fruit {
+
+    protected float weigth;
+
+    public Fruit(float weigth) {
+        this.weigth = weigth;
+    }
+
+    public abstract float getWeigth();
+}
